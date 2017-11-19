72736abfaba146a092b64cff2e8bad28ed06bdaf
jeremy <jeremy@trendiguru.com>
2017-11-19 12:44:04 +0200

    packaging

--- imutils.py
+++ imutils.py
@@ -2584,26 +2584,21 @@ def get_median_image(img_arr_list,visual_output=True):
     return median_image
 
 def clahe_rgb(img_arr):
-
     #-----Converting image to LAB Color model-----------------------------------
     lab= cv2.cvtColor(img_arr, cv2.COLOR_BGR2LAB)
-    cv2.imshow("lab",lab)
-
+#    cv2.imshow("lab",lab)
     #-----Splitting the LAB image to different channels-------------------------
     l, a, b = cv2.split(lab)
-    cv2.imshow('l_channel', l)
-    cv2.imshow('a_channel', a)
-    cv2.imshow('b_channel', b)
-
-    #-----Applying CLAHE to L-channel-------------------------------------------
+    # cv2.imshow('l_channel', l)
+    # cv2.imshow('a_channel', a)
+    # cv2.imshow('b_channel', b)
+    #     #-----Applying CLAHE to L-channel-------------------------------------------
     clahe = cv2.createCLAHE(clipLimit=3.0, tileGridSize=(8,8))
     cl = clahe.apply(l)
 #    cv2.imshow('CLAHE output', cl)
-
     #-----Merge the CLAHE enhanced L-channel with the a and b channel-----------
     limg = cv2.merge((cl,a,b))
  #   cv2.imshow('limg', limg)
-
     #-----Converting image from LAB Color model to RGB model--------------------
     final = cv2.cvtColor(limg, cv2.COLOR_LAB2BGR)
   #  cv2.imshow('final', final)
