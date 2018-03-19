d54f03a0598969550ffb37f8efbe4f462f925973
jeremy-rutman <jeremy_spagnet@hotmail.com>
2018-03-19 20:16:55 +0200

    test median image

--- setup.py
+++ setup.py
@@ -1,19 +1,41 @@
 __author__ = 'jeremy'
-from distutils.core import setup
+'''
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+'''
+
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+
 
 setup(
     name='ml_utils',
     version='0.1.0',
     author='Jeremy Rutman',
     author_email='jeremy.rutman@gmail.com',
-    packages=['ml_utils', 'ml_utils.test'],
-    scripts=['bin/random_augment.py','bin/convert_tf.py'],
+#    packages=['ml_utils', 'ml_utils.test'],
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+ #   scripts=['bin/random_augment.py','bin/convert_tf.py'],
     url='https://github.com/jeremy-rutman/ml-support-code-in-python',
     license='LICENSE.txt',
     description='Useful machine-learning related stuff.',
     long_description=open('README.md').read(),
     install_requires=[
-        "OpenCV >= 2.4.0",
+    #    "opencv-python >= 2.4.0",
         "numpy >= 0.1.4",
     ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3'
+        ],
+    keywords='neural networks support code',
 )
